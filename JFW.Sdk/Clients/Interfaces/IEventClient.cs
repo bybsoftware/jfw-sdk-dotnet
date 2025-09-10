9a9dbb79521b56d1d1074ef59ad171d8078c7367
Steve Bang <mrsteve.bang@gmail.com>
2025-09-10 21:00:43 +0700

    add comment xml header function

--- JFW.Sdk/Clients/Interfaces/IEventClient.cs
+++ JFW.Sdk/Clients/Interfaces/IEventClient.cs
@@ -9,15 +9,22 @@ namespace JFW.Sdk.Clients.Interfaces;
 public interface IEventsClient
 {
     /// <summary>
-    /// 
+    /// Get an event
     /// </summary>
+    /// <remarks>
+    /// This method retrieves an event.
+    /// API Endpoint: GET api/v1/events/{id}
+    /// </remarks>
     /// <param name="id"></param>
     /// <returns></returns>
     Task<Event?> GetByIdAsync(string id);
 
     /// <summary>
-    /// 
+    /// Get events
     /// </summary>
+    /// <remarks>
+    /// API Endpoint: GET api/v1/events
+    /// </remarks>
     /// <param name="request"></param>
     /// <returns></returns>
     Task<PaginationList<Event>?> GetAllAsync(GetEventsRequest request);
